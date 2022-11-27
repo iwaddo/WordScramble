86a9c8ff4992027728011afbc909617cbe280b71
Ian Waddington <iwaddo@icloud.com>
2022-11-27 08:29:33 +0000

    Day 29 before UITextChecker

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -11,15 +11,31 @@ struct ContentView: View {
     
     var body: some View {
         Text("Hello World")
+        
+        
+        
+        
+        
+//        VStack {
+//            Text("Hello World")
+//
+//            let input = "a b c"
+//            let letters = input.components(separatedBy: " ")
+//            Text("Letter 1 is a \(letters[1])")
+//            Text("Hello World 2")
+//        }
     }
+
     
-    func loadFile() {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL) {
-                fileContents
-            }
-        }
-    }
+    
+    
+//    func loadFile() {
+//        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
+//            if let fileContents = try? String(contentsOf: fileURL) {
+//                fileContents
+//            }
+//        }
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -27,3 +43,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
